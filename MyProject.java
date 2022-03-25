73eec9cda2269433cc8a4734f4333e5a96588e49
Narawit <seumpicco@gmail.com>
2022-03-25 09:23:42 +0700

    modified 1

--- MyProject.java
+++ MyProject.java
@@ -16,6 +16,9 @@ public class MyProject {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        System.out.println("Hello world");
+        
     }
     
 }
